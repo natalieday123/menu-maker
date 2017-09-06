c4dca2dbee7a6be1169288633241450e45bb77c3
Natalie <nday2018@ihs.immaculateheart.org>
2017-09-06 08:57:02 -0700

    working menu2

--- menu2.rb
+++ menu2.rb
@@ -4,58 +4,38 @@ puts "Menu:
 3. turkey melt
 4. garden burger
 5. side of fries"
-puts "What would you like?"
-choice = gets.chomp
+# puts "What would you like?"
+# choice = gets.chomp
+answer = "no"
 
-if choice == "cheesy pasta".downcase or choice == "1"
-  puts "You ordered Cheesy Pasta correct?"
-  answer = gets.chomp
-  while !(answer=="yes".downcase)
-    puts "What would you like?"
+while !(answer=="yes".downcase)
+  puts "What would you like?"
+  choice = gets.chomp
+
+  if choice == "cheesy pasta".downcase or choice == "1"
+    puts "You ordered Cheesy Pasta correct?"
     answer = gets.chomp
-    end
-      puts "Thank you!"
-      puts "#{choice}".upcase
-elsif choice == "chicken tenders".downcase
-  puts "You ordered Chicken Tenders correct?"
-  answer = gets.chomp
-  while !(answer=="yes".downcase)
-    puts "What would you like?"
+
+  elsif choice == "chicken tenders".downcase
+    puts "You ordered Chicken Tenders correct?"
     answer = gets.chomp
-    end
-      puts "Thank you!"
-      puts "#{choice}".upcase
-elsif choice == "turkey melt".downcase
-  puts "You ordered the Turkey Melt correct?"
-  answer = gets.chomp
-  while !(answer=="yes".downcase)
-    puts "What would you like?"
+
+  elsif choice == "turkey melt".downcase
+    puts "You ordered the Turkey Melt correct?"
     answer = gets.chomp
-    end
-      puts "Thank you!"
-      puts "#{choice}".upcase
-elsif choice == "garden burger".downcase
-  puts "You ordered the Garden Burger, correct?"
-  answer = gets.chomp
-  while !(answer=="yes".downcase)
-    puts "What would you like?"
+
+  elsif choice == "garden burger".downcase
+    puts "You ordered the Garden Burger, correct?"
     answer = gets.chomp
-    end
-      puts "Thank you!"
-      puts "#{choice}".upcase
-elsif choice == "side of fries".downcase
-  puts "You ordered the side of fries, correct?"
-  answer = gets.chomp
-  while !(answer=="yes".downcase)
-    puts "What would you like?"
+
+  elsif choice == "side of fries".downcase
+    puts "You ordered the side of fries, correct?"
     answer = gets.chomp
-    end
-      puts "Thank you!"
-      puts "#{choice}".upcase
-    # while !(answer=="yes".downcase)
-    #   puts "What would you like?"
-    #   answer = gets.chomp
-    #   end
-    #   puts "Thank you!"
-    #   puts "#{choice}".upcase
+  else
+    puts "That's not an option"
+  end
+
+
 end
+  puts "Thank you!"
+    puts "#{choice}".upcase
